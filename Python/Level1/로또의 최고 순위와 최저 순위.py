0c399fbb9be35ce2ee529dfa8257e9560102feb2
Kwon Youngbin <kyb951020@naver.com>
2021-11-12 14:36:05 +0900

    Test

---
+++ Python/Level1/로또의 최고 순위와 최저 순위.py
@@ -0,0 +1,20 @@
+def solution(lottos, win_nums):
+    answer = []
+    zero_count = lottos.count(0)
+    collect_count = 0
+
+    for i in lottos:
+        if i in win_nums:
+            collect_count += 1
+
+    if zero_count + collect_count >= 2:
+        answer.append(7 - (zero_count + collect_count))
+    else:
+        answer.append(6)
+
+    if collect_count >= 2:
+        answer.append(7 - collect_count)
+    else:
+        answer.append(6)
+
+    return answera
